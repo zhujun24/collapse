639924f879cc7a6dfe9a047becec0fc3fff77dd1
yiminghe <yiminghe@gmail.com>
2015-08-28 01:54:04 +0800

    Updates

--- examples/simple.js
+++ examples/simple.js
@@ -11,11 +11,11 @@ webpackJsonp([0,1],[
 
 	'use strict';
 	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-collapse/assets/index.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	__webpack_require__(2);
-	__webpack_require__(6);
-	var Collapse = __webpack_require__(7);
+	var Collapse = __webpack_require__(3);
 	var Panel = Collapse.Panel;
-	var React = __webpack_require__(9);
+	var React = __webpack_require__(7);
 	
 	var text = '\n  A dog is a type of domesticated animal.\n  Known for its loyalty and faithfulness,\n  it can be found as a welcome guest in many households across the world.\n';
 	
@@ -119,253 +119,6 @@ webpackJsonp([0,1],[
 
 /***/ },
 /* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	// style-loader: Adds some css to the DOM by adding a <style> tag
-	
-	// load the styles
-	var content = __webpack_require__(3);
-	if(typeof content === 'string') content = [[module.id, content, '']];
-	// add the styles to the DOM
-	var update = __webpack_require__(5)(content, {});
-	// Hot Module Replacement
-	if(false) {
-		// When the styles change, update the <style> tags
-		module.hot.accept("!!/Users/eward/code/accordion/node_modules/rc-tools/node_modules/css-loader/index.js!/Users/eward/code/accordion/assets/index.css", function() {
-			var newContent = require("!!/Users/eward/code/accordion/node_modules/rc-tools/node_modules/css-loader/index.js!/Users/eward/code/accordion/assets/index.css");
-			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-			update(newContent);
-		});
-		// When the module is disposed, remove the <style> tags
-		module.hot.dispose(function() { update(); });
-	}
-
-/***/ },
-/* 3 */
-/***/ function(module, exports, __webpack_require__) {
-
-	exports = module.exports = __webpack_require__(4)();
-	exports.push([module.id, ".rc-collapse {\n  background-color: #f4f4f4;\n  border-radius: 3px;\n  border: 1px solid #d9d9d9;\n}\n.rc-collapse > .rc-collapse-item {\n  border-top: 1px solid #d9d9d9;\n}\n.rc-collapse > .rc-collapse-item:first-child {\n  border-top: none;\n}\n.rc-collapse > .rc-collapse-item > .rc-collapse-header {\n  height: 38px;\n  line-height: 38px;\n  text-indent: 16px;\n  color: #666;\n}\n.rc-collapse > .rc-collapse-item > .rc-collapse-header:before {\n  display: inline-block;\n  content: '\\20';\n  width: 0;\n  height: 0;\n  font-size: 0;\n  line-height: 0;\n  border-top: 3px solid transparent;\n  border-bottom: 3px solid transparent;\n  border-left: 4px solid #666666;\n  vertical-align: middle;\n  margin-right: 8px;\n}\n.rc-collapse-content {\n  height: 0;\n  overflow: hidden;\n  color: #666666;\n  padding: 0 16px;\n  background-color: #fff;\n}\n.rc-collapse-content > .rc-collapse-content-box {\n  margin-top: 16px;\n  margin-bottom: 16px;\n}\n.rc-collapse-item:last-child > .rc-collapse-content {\n  border-radius: 0 0 3px 3px;\n}\n.rc-collapse-collapsing {\n  transition-duration: .3s;\n  transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);\n  transition-property: height;\n}\n.rc-collapse-content-active {\n  height: auto;\n}\n.rc-collapse > .rc-collapse-item-active > .rc-collapse-header:before {\n  border-left: 3px solid transparent;\n  border-right: 3px solid transparent;\n  border-top: 4px solid #666666;\n  margin-right: 6px;\n}\n", ""]);
-
-/***/ },
-/* 4 */
-/***/ function(module, exports) {
-
-	module.exports = function() {
-		var list = [];
-		list.toString = function toString() {
-			var result = [];
-			for(var i = 0; i < this.length; i++) {
-				var item = this[i];
-				if(item[2]) {
-					result.push("@media " + item[2] + "{" + item[1] + "}");
-				} else {
-					result.push(item[1]);
-				}
-			}
-			return result.join("");
-		};
-		return list;
-	}
-
-/***/ },
-/* 5 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/*
-		MIT License http://www.opensource.org/licenses/mit-license.php
-		Author Tobias Koppers @sokra
-	*/
-	var stylesInDom = {},
-		memoize = function(fn) {
-			var memo;
-			return function () {
-				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
-				return memo;
-			};
-		},
-		isIE9 = memoize(function() {
-			return /msie 9\b/.test(window.navigator.userAgent.toLowerCase());
-		}),
-		getHeadElement = memoize(function () {
-			return document.head || document.getElementsByTagName("head")[0];
-		}),
-		singletonElement = null,
-		singletonCounter = 0;
-	
-	module.exports = function(list, options) {
-		if(false) {
-			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
-		}
-	
-		options = options || {};
-		// Force single-tag solution on IE9, which has a hard limit on the # of <style>
-		// tags it will allow on a page
-		if (typeof options.singleton === "undefined") options.singleton = isIE9();
-	
-		var styles = listToStyles(list);
-		addStylesToDom(styles, options);
-	
-		return function update(newList) {
-			var mayRemove = [];
-			for(var i = 0; i < styles.length; i++) {
-				var item = styles[i];
-				var domStyle = stylesInDom[item.id];
-				domStyle.refs--;
-				mayRemove.push(domStyle);
-			}
-			if(newList) {
-				var newStyles = listToStyles(newList);
-				addStylesToDom(newStyles, options);
-			}
-			for(var i = 0; i < mayRemove.length; i++) {
-				var domStyle = mayRemove[i];
-				if(domStyle.refs === 0) {
-					for(var j = 0; j < domStyle.parts.length; j++)
-						domStyle.parts[j]();
-					delete stylesInDom[domStyle.id];
-				}
-			}
-		};
-	}
-	
-	function addStylesToDom(styles, options) {
-		for(var i = 0; i < styles.length; i++) {
-			var item = styles[i];
-			var domStyle = stylesInDom[item.id];
-			if(domStyle) {
-				domStyle.refs++;
-				for(var j = 0; j < domStyle.parts.length; j++) {
-					domStyle.parts[j](item.parts[j]);
-				}
-				for(; j < item.parts.length; j++) {
-					domStyle.parts.push(addStyle(item.parts[j], options));
-				}
-			} else {
-				var parts = [];
-				for(var j = 0; j < item.parts.length; j++) {
-					parts.push(addStyle(item.parts[j], options));
-				}
-				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
-			}
-		}
-	}
-	
-	function listToStyles(list) {
-		var styles = [];
-		var newStyles = {};
-		for(var i = 0; i < list.length; i++) {
-			var item = list[i];
-			var id = item[0];
-			var css = item[1];
-			var media = item[2];
-			var sourceMap = item[3];
-			var part = {css: css, media: media, sourceMap: sourceMap};
-			if(!newStyles[id])
-				styles.push(newStyles[id] = {id: id, parts: [part]});
-			else
-				newStyles[id].parts.push(part);
-		}
-		return styles;
-	}
-	
-	function createStyleElement() {
-		var styleElement = document.createElement("style");
-		var head = getHeadElement();
-		styleElement.type = "text/css";
-		head.appendChild(styleElement);
-		return styleElement;
-	}
-	
-	function addStyle(obj, options) {
-		var styleElement, update, remove;
-	
-		if (options.singleton) {
-			var styleIndex = singletonCounter++;
-			styleElement = singletonElement || (singletonElement = createStyleElement());
-			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
-			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
-		} else {
-			styleElement = createStyleElement();
-			update = applyToTag.bind(null, styleElement);
-			remove = function () {
-				styleElement.parentNode.removeChild(styleElement);
-			};
-		}
-	
-		update(obj);
-	
-		return function updateStyle(newObj) {
-			if(newObj) {
-				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
-					return;
-				update(obj = newObj);
-			} else {
-				remove();
-			}
-		};
-	}
-	
-	function replaceText(source, id, replacement) {
-		var boundaries = ["/** >>" + id + " **/", "/** " + id + "<< **/"];
-		var start = source.lastIndexOf(boundaries[0]);
-		var wrappedReplacement = replacement
-			? (boundaries[0] + replacement + boundaries[1])
-			: "";
-		if (source.lastIndexOf(boundaries[0]) >= 0) {
-			var end = source.lastIndexOf(boundaries[1]) + boundaries[1].length;
-			return source.slice(0, start) + wrappedReplacement + source.slice(end);
-		} else {
-			return source + wrappedReplacement;
-		}
-	}
-	
-	function applyToSingletonTag(styleElement, index, remove, obj) {
-		var css = remove ? "" : obj.css;
-	
-		if(styleElement.styleSheet) {
-			styleElement.styleSheet.cssText = replaceText(styleElement.styleSheet.cssText, index, css);
-		} else {
-			var cssNode = document.createTextNode(css);
-			var childNodes = styleElement.childNodes;
-			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
-			if (childNodes.length) {
-				styleElement.insertBefore(cssNode, childNodes[index]);
-			} else {
-				styleElement.appendChild(cssNode);
-			}
-		}
-	}
-	
-	function applyToTag(styleElement, obj) {
-		var css = obj.css;
-		var media = obj.media;
-		var sourceMap = obj.sourceMap;
-	
-		if(sourceMap && typeof btoa === "function") {
-			try {
-				css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(sourceMap)) + " */";
-				css = "@import url(\"data:text/css;base64," + btoa(css) + "\")";
-			} catch(e) {}
-		}
-	
-		if(media) {
-			styleElement.setAttribute("media", media)
-		}
-	
-		if(styleElement.styleSheet) {
-			styleElement.styleSheet.cssText = css;
-		} else {
-			while(styleElement.firstChild) {
-				styleElement.removeChild(styleElement.firstChild);
-			}
-			styleElement.appendChild(document.createTextNode(css));
-		}
-	}
-
-
-/***/ },
-/* 6 */
 /***/ function(module, exports) {
 
 	/*! http://mths.be/repeat v0.2.0 by @mathias */
@@ -421,184 +174,40 @@ webpackJsonp([0,1],[
 
 
 /***/ },
-/* 7 */
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	module.exports = __webpack_require__(4);
+
+/***/ },
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// export this package's api
 	'use strict';
 	
-	module.exports = __webpack_require__(8);
-	module.exports.Panel = __webpack_require__(10);
+	module.exports = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./Collapse\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	module.exports.Panel = __webpack_require__(6);
 
 /***/ },
-/* 8 */
+/* 5 */,
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	var React = __webpack_require__(9);
-	var PropTypes = React.PropTypes;
-	var createClass = React.createClass;
-	var Children = React.Children;
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 	
-	var CollapsePanel = __webpack_require__(10);
-	
-	if (!Array.isArray) {
-	  Array.isArray = function (arg) {
-	    return Object.prototype.toString.call(arg) === '[object Array]';
-	  };
-	}
-	
-	module.exports = createClass({
-	
-	  displayName: 'Collapse',
-	
-	  propTypes: {
-	    prefixCls: PropTypes.string,
-	    activeKey: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
-	    defaultActiveKey: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
-	    onChange: PropTypes.func,
-	    accordion: PropTypes.bool
-	  },
-	
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      prefixCls: 'rc-collapse',
-	      onChange: function onChange() {},
-	      accordion: false
-	    };
-	  },
-	
-	  getInitialState: function getInitialState() {
-	    var _props = this.props;
-	    var defaultActiveKey = _props.defaultActiveKey;
-	    var activeKey = _props.activeKey;
-	    var accordion = _props.accordion;
-	
-	    // If is not accordion mode, then, defaultActiveKey should be an array
-	    if (!accordion) {
-	      defaultActiveKey = defaultActiveKey || [];
-	    }
-	
-	    return {
-	      activeKey: activeKey || defaultActiveKey
-	    };
-	  },
-	
-	  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-	    if ('activeKey' in nextProps) {
-	      this.setState({
-	        activeKey: nextProps.activeKey
-	      });
-	    }
-	  },
-	
-	  handleClickItem: function handleClickItem(key) {
-	    var _this = this;
-	
-	    return function () {
-	      var activeKey = _this._getActivityKey();
-	      if (_this.props.accordion) {
-	        _this.setState({
-	          activeKey: key === activeKey ? null : key
-	        });
-	      } else {
-	
-	        var index = activeKey.indexOf(key);
-	        var isActive = index > -1;
-	        if (isActive) {
-	          // remove active state
-	          activeKey.splice(index, 1);
-	        } else {
-	          activeKey.push(key);
-	        }
-	
-	        _this.setState({ activeKey: activeKey });
-	      }
-	      _this.props.onChange(key);
-	    };
-	  },
-	
-	  _getActivityKey: function _getActivityKey() {
-	    var activeKey = this.state.activeKey;
-	    var accordion = this.props.accordion;
-	
-	    if (accordion && Array.isArray(activeKey)) {
-	      activeKey = activeKey[0];
-	    }
-	
-	    if (!accordion && !Array.isArray(activeKey)) {
-	      activeKey = activeKey ? [activeKey] : [];
-	    }
-	    return activeKey;
-	  },
-	
-	  getItems: function getItems() {
-	    var _this2 = this;
-	
-	    var activeKey = this._getActivityKey();
-	    var _props2 = this.props;
-	    var prefixCls = _props2.prefixCls;
-	    var accordion = _props2.accordion;
-	
-	    return Children.map(this.props.children, function (child, i) {
-	      // If there is no key provide, use the panel order as default key
-	      var key = child.key || i;
-	      var header = child.props.header;
-	      var isActive = false;
-	      if (accordion) {
-	        isActive = activeKey === key;
-	      } else {
-	        isActive = activeKey.indexOf(key) > -1;
-	      }
-	
-	      var props = {
-	        key: key,
-	        header: header,
-	        isActive: isActive,
-	        prefixCls: prefixCls,
-	        children: child.props.children,
-	        onItemClick: _this2.handleClickItem(key).bind(_this2)
-	      };
-	
-	      return React.createElement(CollapsePanel, props);
-	    });
-	  },
-	
-	  render: function render() {
-	    var prefixCls = this.props.prefixCls;
-	    return React.createElement('div', { className: prefixCls }, this.getItems());
-	  }
-	});
-
-/***/ },
-/* 9 */
-/***/ function(module, exports) {
-
-	module.exports = React;
-
-/***/ },
-/* 10 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	function _defineProperty(obj, key, value) {
-	  if (key in obj) {
-	    Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
-	  } else {
-	    obj[key] = value;
-	  }return obj;
-	}
-	
-	var React = __webpack_require__(9);
+	var React = __webpack_require__(7);
 	var PropTypes = React.PropTypes;
 	var createClass = React.createClass;
 	var findDOMNode = React.findDOMNode;
 	
-	var classnames = __webpack_require__(11);
-	var cssAnimation = __webpack_require__(12);
-	var event = __webpack_require__(13);
+	var classnames = __webpack_require__(8);
+	var cssAnimation = __webpack_require__(9);
+	var event = __webpack_require__(10);
 	var isSupportCssAnimate = event.endEvents.length > 0;
 	
 	module.exports = createClass({
@@ -640,8 +249,26 @@ webpackJsonp([0,1],[
 	    var contentCls = classnames((_classnames = {}, _defineProperty(_classnames, prefixCls + '-content', true), _defineProperty(_classnames, prefixCls + '-content-active', isActive), _classnames));
 	    var itemCls = classnames((_classnames2 = {}, _defineProperty(_classnames2, prefixCls + '-item', true), _defineProperty(_classnames2, prefixCls + '-item-active', isActive), _classnames2));
 	
-	    return React.createElement('div', { className: itemCls }, React.createElement('div', { className: headerCls, onClick: this.handleItemClick,
-	      role: 'tab', 'aria-expanded': isActive }, header), React.createElement('div', { className: contentCls, ref: 'content', role: 'tabpanel' }, React.createElement('div', { className: prefixCls + '-content-box' }, children)));
+	    return React.createElement(
+	      'div',
+	      { className: itemCls },
+	      React.createElement(
+	        'div',
+	        { className: headerCls, onClick: this.handleItemClick,
+	          role: 'tab', 'aria-expanded': isActive },
+	        React.createElement('i', { className: 'arrow' }),
+	        header
+	      ),
+	      React.createElement(
+	        'div',
+	        { className: contentCls, ref: 'content', role: 'tabpanel' },
+	        React.createElement(
+	          'div',
+	          { className: prefixCls + '-content-box' },
+	          children
+	        )
+	      )
+	    );
 	  },
 	
 	  componentDidMount: function componentDidMount() {
@@ -652,7 +279,6 @@ webpackJsonp([0,1],[
 	  },
 	
 	  componentDidUpdate: function componentDidUpdate(prevProps) {
-	
 	    var isActive = this.props.isActive;
 	
 	    // no change
@@ -666,7 +292,7 @@ webpackJsonp([0,1],[
 	  _anim: function _anim(opacity) {
 	    var el = findDOMNode(this.refs.content);
 	    if (!isSupportCssAnimate) {
-	      el.style.height = opacity ? 'auto' : 0;
+	      el.style.height = opacity ? 0 : '';
 	      return;
 	    }
 	
@@ -687,11 +313,16 @@ webpackJsonp([0,1],[
 	      cssAnimation.removeClass(el, collapsing);
 	    });
 	  }
-	
 	});
 
 /***/ },
-/* 11 */
+/* 7 */
+/***/ function(module, exports) {
+
+	module.exports = React;
+
+/***/ },
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -746,13 +377,13 @@ webpackJsonp([0,1],[
 
 
 /***/ },
-/* 12 */
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	var Event = __webpack_require__(13);
-	var Css = __webpack_require__(14);
+	var Event = __webpack_require__(10);
+	var Css = __webpack_require__(11);
 	
 	var cssAnimation = function cssAnimation(node, transitionName, callback) {
 	  var className = transitionName;
@@ -845,7 +476,7 @@ webpackJsonp([0,1],[
 	module.exports = cssAnimation;
 
 /***/ },
-/* 13 */
+/* 10 */
 /***/ function(module, exports) {
 
 	
@@ -932,7 +563,7 @@ webpackJsonp([0,1],[
 	module.exports = TransitionEvents;
 
 /***/ },
-/* 14 */
+/* 11 */
 /***/ function(module, exports) {
 
 	'use strict';
